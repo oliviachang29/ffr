60d93503a766fcf8c422a523ad8e84bf4de613c0
Olivia <15709895+oliviachang29@users.noreply.github.com>
2023-01-22 16:15:22 -0500

    add adv board

--- publish.sh
+++ publish.sh
@@ -1,17 +1,17 @@
+# doesn't work when you run from shell.... :( but works if you just copy paste
 # commit to main directory
-# git add -A
-# git commit -m $1
-# git push
+git add -A
+git commit -m $1
+git push
 
 # build and copy over
-# npm run-script build
-shopt -s extglob
+npm run-script build
 cd fossil_free_research && rm -r !(CNAME|.git|.gitignore)
-# cd ..
-# cp -R dist/. fossil_free_research/
-# cd fossil_free_research
+cd ..
+cp -R dist/. fossil_free_research/
+cd fossil_free_research
 
-# # commit to subdirectory
-# git add -A
-# git commit -m $1
-# git push
+# commit to subdirectory
+git add -A
+git commit -m $1
+git push
